1426bd6a864f214ec5c10ae1dd258ed87f360966
Matthew-PT <matthew.brown@pridetronics.com>
2024-03-04 22:55:57 -0700

    Merge cleanup

--- src/main/java/frc/robot/commands/FieldPositionUpdate.java
+++ src/main/java/frc/robot/commands/FieldPositionUpdate.java
@@ -8,7 +8,6 @@ import java.util.Optional;
 
 import org.photonvision.EstimatedRobotPose;
 
-import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
