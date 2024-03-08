24a40eae4cd9f83950445dd6d983f7e9a177c936
Matthew-PT <matthew.brown@pridetronics.com>
2024-03-07 22:17:57 -0700

    removed a previous fix due to other issues

--- src/main/java/frc/robot/utils/TrajectoryHelper.java
+++ src/main/java/frc/robot/utils/TrajectoryHelper.java
@@ -48,11 +48,10 @@ public class TrajectoryHelper {
     }
 
     public static Trajectory createTrajectoryWithAllianceRelativePositioning(Pose2d init, List<Translation2d> points, Pose2d end) {
-        List<Translation2d> newPointList = new ArrayList<Translation2d>(points.size());
+        List<Translation2d> newPointList = new ArrayList<Translation2d>();
         for (int i = 0; i < points.size(); i++) {
-            if (points.size() == 0) break;
             
-            newPointList.set(i, 
+            newPointList.add( 
                 toAllianceRelativePosition(
                     points.get(i)
                 )
